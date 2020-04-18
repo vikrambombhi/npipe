454904bab1bc1fbb4dabd6bf9b3b06f0979ba741
vikrambombhi <vbombhi@gmail.com>
2020-04-18 18:34:57 -0400

    supervisor tree + handle multiple connections

--- lib/create_pipe.ex
+++ lib/create_pipe.ex
@@ -1,23 +1,20 @@
 defmodule CreatePipe do
   require Logger
 
+  @doc """
+  Starts accepting connections on the given `port`.
+  """
   def accept(port) do
-    # The options below mean:
-    #
-    # 1. `:binary` - receives data as binaries (instead of lists)
-    # 2. `packet: :line` - receives data line by line
-    # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
-    # 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
-    #
-    {:ok, socket} =
-      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
-    Logger.info("Accepting connections on port #{port}")
+    {:ok, socket} = :gen_tcp.listen(port,
+      [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
     loop_acceptor(socket)
   end
 
   defp loop_acceptor(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    serve(client)
+    {:ok, pid} = Task.Supervisor.start_child(CreatePipe.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
     loop_acceptor(socket)
   end
 
@@ -38,4 +35,3 @@ defmodule CreatePipe do
     :gen_tcp.send(socket, line)
   end
 end
-
--- lib/npipe.ex
+++ lib/npipe.ex
@@ -1,4 +1,7 @@
 defmodule NPIPE do
+  use Application
+  require Logger
+
   @moduledoc """
   Documentation for `NPIPE`.
   """
@@ -8,11 +11,24 @@ defmodule NPIPE do
 
   ## Examples
 
-      iex> NPIPE.hello()
-      :world
+  iex> NPIPE.hello()
+  :world
 
   """
   def hello do
     :world
+  end
+
+  def start(_type, _args) do
+    Logger.info "Starting npipe"
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
+    children = [
+      {Task.Supervisor, name: CreatePipe.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> CreatePipe.accept(port) end}, restart: :permanent)
+    ]
+
+    opts = [strategy: :one_for_one, name: CreatePipe.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
